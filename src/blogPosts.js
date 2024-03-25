369ec1f6a5ccba73d5c3ec3573f8af8f655161c5
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-03-25 12:09:34 +0100

    commit

--- src/blogPosts.js
+++ src/blogPosts.js
@@ -1,59 +1,123 @@
 export const blogPosts = [
   {
-    title: ['Miriam Post'],
-    link: ['https://miriamsterl.netlify.app/blog/miriam-post'],
-    guid: ['https://your-docusaurus-site.example.com/blog/miriam-post'],
-    pubDate: ['Wed, 20 Mar 2024 00:00:00 GMT'],
-    description: ['hier ein kurzes update'],
-    'content:encoded': ['<p>#Großer Titel\nhier ein kurzes update</p>'],
-    category: ['hola', 'docusaurus'],
+    "title": [
+      "Miriam Post"
+    ],
+    "link": [
+      "https://miriamsterl.netlify.app/blog/miriam-post"
+    ],
+    "guid": [
+      "https://miriamsterl.netlify.app/blog/miriam-post"
+    ],
+    "pubDate": [
+      "Wed, 20 Mar 2024 00:00:00 GMT"
+    ],
+    "description": [
+      "hier ein kurzes update"
+    ],
+    "content:encoded": [
+      "<p>#Großer Titel\nhier ein kurzes update</p>"
+    ],
+    "category": [
+      "hola",
+      "docusaurus"
+    ]
   },
   {
-    title: ['RSS TIMEOUT'],
-    link: ['https://miriamsterl.netlify.app/blog/rss-timeout'],
-    guid: ['https://your-docusaurus-site.example.com/blog/rss-timeout'],
-    pubDate: ['Mon, 18 Mar 2024 00:00:00 GMT'],
-    description: ['RSS TESTING.'],
-    'content:encoded': ['<p>RSS TESTING.</p>'],
-    category: ['test', 'rss'],
+    "title": [
+      "RSS TIMEOUT"
+    ],
+    "link": [
+      "https://miriamsterl.netlify.app/blog/rss-timeout"
+    ],
+    "guid": [
+      "https://miriamsterl.netlify.app/blog/rss-timeout"
+    ],
+    "pubDate": [
+      "Mon, 18 Mar 2024 00:00:00 GMT"
+    ],
+    "description": [
+      "RSS TESTING."
+    ],
+    "content:encoded": [
+      "<p>RSS TESTING.</p>"
+    ],
+    "category": [
+      "test",
+      "rss"
+    ]
   },
   {
-    title: ['New Post Update Test'],
-    link: ['https://your-docusaurus-site.example.com/blog/new-post'],
-    guid: ['https://your-docusaurus-site.example.com/blog/new-post'],
-    pubDate: ['Mon, 18 Mar 2024 00:00:00 GMT'],
-    description: [
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet',
+    "title": [
+      "New Post Update Test"
     ],
-    'content:encoded': [
-      '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet</p>',
+    "link": [
+      "https://miriamsterl.netlify.app/blog/new-post"
     ],
-    category: ['hola', 'docusaurus'],
+    "guid": [
+      "https://miriamsterl.netlify.app/blog/new-post"
+    ],
+    "pubDate": [
+      "Mon, 18 Mar 2024 00:00:00 GMT"
+    ],
+    "description": [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet"
+    ],
+    "content:encoded": [
+      "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet</p>"
+    ],
+    "category": [
+      "hola",
+      "docusaurus"
+    ]
   },
   {
-    title: ['Welcome'],
-    link: ['https://your-docusaurus-site.example.com/blog/welcome'],
-    guid: ['https://your-docusaurus-site.example.com/blog/welcome'],
-    pubDate: ['Thu, 26 Aug 2021 00:00:00 GMT'],
-    description: [
-      'Docusaurus blogging features are powered by the blog plugin.',
+    "title": [
+      "Welcome"
     ],
-    'content:encoded': [
-      '<p><a href="https://docusaurus.io/docs/blog" target="_blank" rel="noopener noreferrer">Docusaurus blogging features</a> are powered by the <a href="https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-blog" target="_blank" rel="noopener noreferrer">blog plugin</a>.</p>\n<p>Test ob es neu deployed Simply add Markdown files (or folders) to the <code>blog</code> directory.</p>\n<p>Regular blog authors can be added to <code>authors.yml</code>.</p>\n<p>The blog post date can be extracted from filenames, such as:</p>\n<ul>\n<li><code>2019-05-30-welcome.md</code></li>\n<li><code>2019-05-30-welcome/index.md</code></li>\n</ul>\n<p>A blog post folder can be convenient to co-locate blog post images:</p>\n<p><img decoding="async" loading="lazy" alt="Docusaurus Plushie" src="https://your-docusaurus-site.example.com/assets/images/docusaurus-plushie-banner-a60f7593abca1e3eef26a9afa244e4fb.jpeg" width="1500" height="500" class="img_ev3q"></p>\n<p>The blog supports tags as well!</p>\n<p><strong>And if you don\'t want a blog</strong>: just delete this directory, and use <code>blog: false</code> in your Docusaurus config.</p>',
+    "link": [
+      "https://miriamsterl.netlify.app/blog/welcome"
     ],
-    category: ['facebook', 'hello', 'docusaurus'],
+    "guid": [
+      "https://miriamsterl.netlify.app/blog/welcome"
+    ],
+    "pubDate": [
+      "Thu, 26 Aug 2021 00:00:00 GMT"
+    ],
+    "description": [
+      "Docusaurus blogging features are powered by the blog plugin."
+    ],
+    "content:encoded": [
+      "<p><a href=\"https://docusaurus.io/docs/blog\" target=\"_blank\" rel=\"noopener noreferrer\">Docusaurus blogging features</a> are powered by the <a href=\"https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-blog\" target=\"_blank\" rel=\"noopener noreferrer\">blog plugin</a>.</p>\n<p>Test ob es neu deployed Simply add Markdown files (or folders) to the <code>blog</code> directory.</p>\n<p>Regular blog authors can be added to <code>authors.yml</code>.</p>\n<p>The blog post date can be extracted from filenames, such as:</p>\n<ul>\n<li><code>2019-05-30-welcome.md</code></li>\n<li><code>2019-05-30-welcome/index.md</code></li>\n</ul>\n<p>A blog post folder can be convenient to co-locate blog post images:</p>\n<p><img decoding=\"async\" loading=\"lazy\" alt=\"Docusaurus Plushie\" src=\"https://miriamsterl.netlify.app/assets/images/docusaurus-plushie-banner-a60f7593abca1e3eef26a9afa244e4fb.jpeg\" width=\"1500\" height=\"500\" class=\"img_ev3q\"></p>\n<p>The blog supports tags as well!</p>\n<p><strong>And if you don't want a blog</strong>: just delete this directory, and use <code>blog: false</code> in your Docusaurus config.</p>"
+    ],
+    "category": [
+      "facebook",
+      "hello",
+      "docusaurus"
+    ]
   },
   {
-    title: ['First Blog Post'],
-    link: ['https://your-docusaurus-site.example.com/blog/first-blog-post'],
-    guid: ['https://your-docusaurus-site.example.com/blog/first-blog-post'],
-    pubDate: ['Tue, 28 May 2019 00:00:00 GMT'],
-    description: [
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet',
+    "title": [
+      "First Blog Post"
     ],
-    'content:encoded': [
-      '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet</p>',
+    "link": [
+      "https://miriamsterl.netlify.app/blog/first-blog-post"
     ],
-    category: ['hola', 'docusaurus'],
-  },
+    "guid": [
+      "https://miriamsterl.netlify.app/blog/first-blog-post"
+    ],
+    "pubDate": [
+      "Tue, 28 May 2019 00:00:00 GMT"
+    ],
+    "description": [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet"
+    ],
+    "content:encoded": [
+      "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet</p>"
+    ],
+    "category": [
+      "hola",
+      "docusaurus"
+    ]
+  }
 ];
