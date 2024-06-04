a4cd7a7ba779d7aafd00cdd179cc91842cb34cad
Miriam Sterl <42772450+MiriamSterl@users.noreply.github.com>
2024-06-04 13:24:10 +0200

    Test title tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Miriam Sterl, Oceanographer',
-  tagline: 'Miriam Sterl, Oceanographer',
+  tagline: 'Miriam Sterl, Oceanograaf',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
